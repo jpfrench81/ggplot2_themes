1ce4a346888d9f9d78b80b54cb1cfb9cff82975a
Joshua French <joshua.french@gmail.com>
2013-08-07 14:27:26 -0600

    version 0.0.1
    Removed reduncies, corrected typo for collor of y-axis, allowed
    addition of options.

--- ggplot2_themes.R
+++ ggplot2_themes.R
@@ -1,14 +1,17 @@
+library(ggplot2)
+library(grid)
+
 theme_R_default <- function(...)
 {
 	theme(
 	panel.background = element_rect(fill='white', colour='black'),
 	panel.grid.major = element_blank(),
-	panel.grid.minor=element_blank(), 
-	axis.line = element_line(), 
+	panel.grid.minor = element_blank(), 
 	axis.title.x = element_text(colour = "black"), 
 	axis.title.y = element_text(colour = "black"), 
 	axis.text.x = element_text(colour = 'black'),
-	axis.text.x = element_text(colour = 'black'))
+	axis.text.y = element_text(colour = 'black'))
+	theme_update(...)
 }
 
 #theme2
