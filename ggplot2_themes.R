e9c4be591b73051b4b7f953b36b900cbbbf346b9
Joshua French <joshua.french@gmail.com>
2013-08-07 12:55:05 -0600

    Version 0.0.0

---
+++ ggplot2_themes.R
@@ -0,0 +1,14 @@
+theme_R_default <- function(...)
+{
+	theme(
+	panel.background = element_rect(fill='white', colour='black'),
+	panel.grid.major = element_blank(),
+	panel.grid.minor=element_blank(), 
+	axis.line = element_line(), 
+	axis.title.x = element_text(colour = "black"), 
+	axis.title.y = element_text(colour = "black"), 
+	axis.text.x = element_text(colour = 'black'),
+	axis.text.x = element_text(colour = 'black'))
+}
+
+#theme2
