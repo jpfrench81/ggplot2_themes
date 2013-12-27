164b1d6e800889819d990c00e3ebda05f7fd7dd0
Joshua French <joshua.french@gmail.com>
2013-12-27 11:45:28 -0700

    0.1.1
    Something that actually works

--- ggplot2_themes.R
+++ ggplot2_themes.R
@@ -4,14 +4,12 @@ library(grid)
 theme_R_default <- function(...)
 {
 	theme(
-	panel.background = element_rect(fill='white', colour='black'),
-	panel.grid.major = element_blank(),
-	panel.grid.minor = element_blank(), 
-	axis.title.x = element_text(colour = "black"), 
-	axis.title.y = element_text(colour = "black"), 
-	axis.text.x = element_text(colour = 'black'),
-	axis.text.y = element_text(colour = 'black'))
-	theme_update(...)
+		panel.background = element_rect(fill='white', colour='black'), 
+		panel.grid.major = element_blank(),
+		panel.grid.minor = element_blank(),
+		axis.title.x = element_text(colour = "black"), 
+		axis.title.y = element_text(colour = "black"),
+		axis.text.x = element_text(colour = 'black'),
+		axis.text.y = element_text(colour = 'black') + theme_update(...)
+	)
 }
-
-#theme2
